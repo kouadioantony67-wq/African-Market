415ba13d9318023c9ff5c39e2ca71913494327d6
kouadioantony67-wq <kouadioantony67@gmail.com>
2026-06-28 13:41:17 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template_string, request, redirect, url_for
 
-app = Flask(_name_)
+app = Flask(__name__)
 
 # Le dictionnaire pour stocker les produits
 PRODUITS = {}
